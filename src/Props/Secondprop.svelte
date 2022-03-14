10f8494687e2798d1279b21ae0bb5959a965b61f
AlexFriis <alex3567@stud.kea.dk>
2022-03-14 10:07:13 +0100

    Lets try again

---
+++ src/Props/Secondprop.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let model;
+    export let horsepower;
+    export let topspeed;
+</script>
+
+<p>
+    the <code>{model}</code> is a beast of a car sporting {horsepower} horsepower and has a topspeed of {topspeed}.
+</p>
